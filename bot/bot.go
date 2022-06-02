1ed5ce0d9d8f3e28248844c28457b7a05ab336fd
Kvertinum01 <mr_matvey444@mail.ru>
2022-06-02 20:01:24 +0300

    bot routes

--- bot/bot.go
+++ bot/bot.go
@@ -7,8 +7,12 @@ import (
 )
 
 type Bot struct {
-	Api      *api.Api
-	Longpoll *polling.Longpoll
+	Api           *api.Api
+	Longpoll      *polling.Longpoll
+	Routes        map[string]func(object.NewMessage)
+	PrivateRoutes map[string]func(object.NewMessage)
+	ChatRoutes    map[string]func(object.NewMessage)
+	AllHandler    func(object.NewMessage)
 }
 
 func NewBot(token string) (*Bot, error) {
@@ -25,8 +29,51 @@ func NewBot(token string) (*Bot, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &Bot{
-		Api:      api,
-		Longpoll: lp,
-	}, nil
+
+	bot := &Bot{
+		Api:           api,
+		Longpoll:      lp,
+		Routes:        make(map[string]func(object.NewMessage)),
+		PrivateRoutes: make(map[string]func(object.NewMessage)),
+		ChatRoutes:    make(map[string]func(object.NewMessage)),
+	}
+	bot.Longpoll.Routes.MessageNew = bot.messageHandler
+
+	return bot, nil
+}
+
+func (bot *Bot) messageHandler(message object.NewMessage) {
+	f, ok := bot.Routes[message.Text]
+	if ok {
+		f(message)
+		return
+	}
+	if message.PeerID < 2000000000 {
+		pf, ok := bot.PrivateRoutes[message.Text]
+		if ok {
+			pf(message)
+			return
+		}
+	} else {
+		cf, ok := bot.ChatRoutes[message.Text]
+		if ok {
+			cf(message)
+			return
+		}
+	}
+	if bot.AllHandler != nil {
+		bot.AllHandler(message)
+	}
+}
+
+func (bot *Bot) OnMessage(text string, f func(object.NewMessage)) {
+	bot.Routes[text] = f
+}
+
+func (bot *Bot) OnPrivateMessage(text string, f func(object.NewMessage)) {
+	bot.PrivateRoutes[text] = f
+}
+
+func (bot *Bot) OnChatMessage(text string, f func(object.NewMessage)) {
+	bot.ChatRoutes[text] = f
 }
